2117ad8f45765e284af33cd42d1b6cccea7d6797
Konrad Kostępski <kkostepski@users.noreply.github.com>
2020-02-15 10:30:53 +0100

    Change font to Roboto

--- src/components/GlobalStyle/GlobalStyle.js
+++ src/components/GlobalStyle/GlobalStyle.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap');
+
   * {
     margin: 0;
     padding: 0;
@@ -8,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    font-family: 'Roboto Condensed', sans-serif;
     background-color: ${props => props.theme.backgroundColor};
     color: ${props => props.theme.fontColor};
   }
