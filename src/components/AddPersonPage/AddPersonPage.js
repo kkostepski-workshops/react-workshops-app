ea582fd156927758353252a25387de94bd6c6877
Konrad Kostępski <kkostepski@users.noreply.github.com>
2020-02-18 00:09:50 +0100

    validation

--- src/components/AddPersonPage/AddPersonPage.js
+++ src/components/AddPersonPage/AddPersonPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Formik, Form } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 
 import Page from '../../layouts/Page';
 import Heading from '../../layouts/Heading';
 import TextField from '../../components/form/TextField';
 import Select from '../../components/form/Select';
 import Button from '../../components/Button';
+import validator, { email, required } from '../../components/form/validators';
 
 const AddPersonPage = () => (
   <Page hasNavigation>
@@ -19,14 +20,38 @@ const AddPersonPage = () => (
     >
       {() => (
         <Form>
-          <TextField name="name" label="Name" />
-          <TextField name="surname" label="Surname" />
-          <TextField name="email" label="Email" />
-          <Select name="gender" label="Gender">
+          <TextField
+            name="name"
+            label="Name"
+            validate={value => validator(value, [required])}
+          />
+          <ErrorMessage name="name" component="div" />
+
+          <TextField
+            name="surname"
+            label="Surname"
+            validate={value => validator(value, [required])}
+          />
+          <ErrorMessage name="surname" component="div" />
+
+          <TextField
+            name="email"
+            label="Email"
+            validate={value => validator(value, [required, email])}
+          />
+          <ErrorMessage name="email" component="div" />
+
+          <Select
+            name="gender"
+            label="Gender"
+            validate={value => validator(value, [required])}
+          >
             <option value="">-- select --</option>
             <option value="male">male</option>
             <option value="female">female</option>
           </Select>
+          <ErrorMessage name="gender" component="div" />
+
           <br />
           <Button type="submit">Add</Button>
         </Form>
