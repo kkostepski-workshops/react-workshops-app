f75d153ffc3409a41c7de3f93c0ca41e4ee228df
Konrad Kostępski <kkostepski@users.noreply.github.com>
2020-04-08 17:55:51 +0200

    API

--- src/components/PeoplePage/PeoplePage.js
+++ src/components/PeoplePage/PeoplePage.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import Page from '../../layouts/Page/Page';
 
-const PeoplePage = () => <Page hasNavigation>People page</Page>;
+const PeoplePage = () => (
+  <Page hasNavigation>
+    <h1>People page</h1>
+
+    
+  </Page>
+);
 
 export default PeoplePage;
